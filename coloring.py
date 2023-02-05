35cede026c5143183f7f22ed6c2355a662cecbf5
naani <nandi.zalka@gmail.com>
2023-02-05 20:02:19 +0100

    improved grey

--- coloring.py
+++ coloring.py
@@ -9,7 +9,7 @@ for x in range(image.size[0]):
     for y in range(image.size[1]):
         (r, g, b,) = pixels[x, y]
 
-        grey = int((r + g + b) / 3)
+        grey = int(0.299 * r + 0.587 * g + 0.114 * b)
 
         pixels[x, y] = (grey, grey, grey)
 image.show()
